4777d33b58938f76dc50349d28c2ac95baac50ea
Rhineson <rkrx123@gmail.com>
2026-08-18 15:54:09 +0800

    Rename chatbot from Duke to Caitlyn

---
+++ src/main/java/Caitlyn.java
@@ -0,0 +1,19 @@
+/**
+ * Entry point for the chatbot application.
+ */
+public class Caitlyn {
+    /**
+     * Starts the application and prints the chatbot banner.
+     *
+     * @param args command-line arguments supplied when the program starts
+     */
+    public static void main(String[] args) {
+        String banner = "  ____      _ _   _             \n"
+                + " / ___|__ _(_) |_| |_   _ _ __  \n"
+                + "| |   / _` | | __| | | | | '_ \\ \n"
+                + "| |__| (_| | | |_| | |_| | | | |\n"
+                + " \\____\\__,_|_|\\__|_|\\__, |_| |_|\n"
+                + "                    |___/       \n";
+        System.out.println(banner);
+    }
+}
